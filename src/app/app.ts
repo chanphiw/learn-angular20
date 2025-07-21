5f47a270071c91a89bce1b1eee254b48e412fd53
chanphiw <86280262+chanphiw@users.noreply.github.com>
2025-07-21 20:04:22 +0700

    two way binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,14 @@
 import { Component, signal } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
  
-  name = signal('TEE');
-
-  showMessage() {
-    alert("Hello, Angular!" + this.name());
-  }
+  username = ('admin TEE');
  
 }
