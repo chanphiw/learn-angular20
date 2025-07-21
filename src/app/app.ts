d59d7a5096f4df2382603805cafef3370af12910
chanphiw <86280262+chanphiw@users.noreply.github.com>
2025-07-21 20:46:33 +0700

    for loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,6 +9,12 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
  
- userPermission = "admin";
+data = [
+  { id: 1, name: 'John', salary: 50000 },
+  { id: 2, name: 'Jane', salary: 60000 },
+  { id: 3, name: 'Bob', salary: 55000 },
+  { id: 4, name: 'Alice', salary: 70000 },
+  { id: 5, name: 'Charlie', salary: 65000 }
+]
 
 }
