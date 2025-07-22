4521f95d11362b879af202ea38ca61892c099f6c
chanphiw <86280262+chanphiw@users.noreply.github.com>
2025-07-22 19:57:57 +0700

    parent to child

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,49 +1,14 @@
 import { Component } from '@angular/core';
-import { signal } from '@angular/core';
+import { Header } from "./header/header";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  fname = signal<string>('Pathompong');
-  lname = signal<string>('Chanphiw');
-  exp = signal<number>(15);
-  skills = signal<string[]>(['Angular', 'React', 'Vue']);
-
-
-change(){
-  this.fname.set('John');
-  this.lname.set('Doe');
-  this.exp.set(10);
-  this.skills.set(['JavaScript', 'TypeScript', 'Python']);
-
-}
-
-increment(){
-  // add max value = 35years
-  const value = this.exp();
-  if (value >= 35) {
-    console.warn('Maximum experience limit reached');
-    return;
-  }
-  this.exp.update(value => value + 1);
-
-  // when max value alerts, you are retired
-  if (value + 1 === 35) {
-    alert('You are retired now');
-  }
-
-}
-
-decrement(){
-  // add min value = 0
-  if (this.exp() > 0) {
-    this.exp.update(value => value - 1);
-  }
-}
+  
 }
---
+++ src/app/header/header.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+  title = input<string>('Header Component');
+}
+
+
+
