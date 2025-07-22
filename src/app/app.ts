d714f14e0f8922cd0404c47c0225f9e71f3000d2
chanphiw <86280262+chanphiw@users.noreply.github.com>
2025-07-22 10:31:42 +0700

    for loop2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -14,7 +14,15 @@ data = [
   { id: 2, name: 'Jane', salary: 60000 },
   { id: 3, name: 'Bob', salary: 55000 },
   { id: 4, name: 'Alice', salary: 70000 },
-  { id: 5, name: 'Charlie', salary: 65000 }
+  { id: 5, name: 'Charlie', salary: 65000 },
+  { id: 6, name: 'David', salary: 72000 },
+  { id: 7, name: 'Eve', salary: 48000 },
 ]
 
+clearData() {
+  this.data = [];
+}
+addData() {
+  this.data.push({ id: this.data.length + 1, name: `New User ${this.data.length + 1}`, salary: Math.floor(Math.random() * 100000) });
+}
 }
