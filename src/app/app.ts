67efac522f98f3482badb35904c257ea4e99c024
chanphiw <86280262+chanphiw@users.noreply.github.com>
2025-07-22 11:56:18 +0700

    NgStyle

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,44 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle  } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
- 
-data = [
-  { id: 1, name: 'John', salary: 50000 },
-  { id: 2, name: 'Jane', salary: 60000 },
-  { id: 3, name: 'Bob', salary: 55000 },
-  { id: 4, name: 'Alice', salary: 70000 },
-  { id: 5, name: 'Charlie', salary: 65000 },
-  { id: 6, name: 'David', salary: 72000 },
-  { id: 7, name: 'Eve', salary: 48000 },
-]
+  data = [
+    { id: 1, name: 'John', salary: 50000 },
+    { id: 2, name: 'Jane', salary: 60000 },
+    { id: 3, name: 'Bob', salary: 55000 },
+    { id: 4, name: 'Alice', salary: 70000 },
+    { id: 5, name: 'Charlie', salary: 65000 },
+    { id: 6, name: 'David', salary: 72000 },
+    { id: 7, name: 'Eve', salary: 48000 },
+  ];
 
-clearData() {
-  this.data = [];
-}
-addData() {
-  this.data.push({ id: this.data.length + 1, name: `New User ${this.data.length + 1}`, salary: Math.floor(Math.random() * 100000) });
-}
+  clearData() {
+    this.data = [];
+  }
+  addData() {
+    this.data.push({
+      id: this.data.length + 1,
+      name: `New User ${this.data.length + 1}`,
+      salary: Math.floor(Math.random() * 100000),
+    });
+  }
 
-isActive = false;
+  isActive = false;
 
-changeMode() {
-  this.isActive = !this.isActive;
-  document.body.classList.toggle('dark', this.isActive);
-  document.body.classList.toggle('light', !this.isActive);
-}
+  changeMode() {
+    this.isActive = !this.isActive;
+    document.body.classList.toggle('dark', this.isActive);
+    document.body.classList.toggle('light', !this.isActive);
+  }
+
+  color = 'purple';
+  size = 30;
+
 }
